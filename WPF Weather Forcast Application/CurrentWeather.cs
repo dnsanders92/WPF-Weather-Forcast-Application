77bbf75d3a8b420b3805325200b72c501737cd33
David Sanders <Sanders-D1@ulster.ac.uk>
2025-04-03 20:44:39 +0100

    Refractored code and added ICON from api. App now display and image for the current conditions.

--- WPF Weather Forcast Application/CurrentWeather.cs
+++ WPF Weather Forcast Application/CurrentWeather.cs
@@ -2,34 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
 using Newtonsoft.Json.Linq;
-
 
 namespace WPF_Weather_Forcast_Application
 {
-    class OpenWeatherAPI
+    class CurrentWeather
     {
-        private string city = "London";
-        private double currentTemperature;
-        private double currentHumidity;
-        private double currentPressure;
+        private double currentTemperature, currentHumidity, currentPressure;
         private string weatherConditions;
-        private string weatherConditionsIMG;
+        private string weatherICON;
         public double CurrentTemperature { get { return currentTemperature; } set { currentTemperature = value; } }
-        public double CurrentHumidity {  get { return currentHumidity; } set { currentHumidity = value; } }
-        public double CurrentPressure {  get { return currentPressure; } set { currentPressure = value; } }
+        public double CurrentHumidity { get { return currentHumidity; } set { currentHumidity = value; } }
+        public double CurrentPressure { get { return currentPressure; } set { currentPressure = value; } }
         public string WeatherConditions { get { return weatherConditions; } set { weatherConditions = value; } }
-        public string WeatherConditionsIMG { get { return weatherConditionsIMG; } set { weatherConditionsIMG = value; } }
-        public string City { get { return city; } set { city = value; } }
-        public async Task FetchCurrentWeather()
+        public string WeatherICON { get { return weatherICON; } set { weatherICON = value; } }
+
+        public async Task FetchCurrentWeather(string lat, string lon)
         {
 
             string apiKey = "236908b3196ed5091106391e27eca5c9";
-            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
 
             using (HttpClient client = new HttpClient())
             {
@@ -37,14 +32,13 @@ namespace WPF_Weather_Forcast_Application
                 string json = await response.Content.ReadAsStringAsync();
                 JObject weatherData = JObject.Parse(json);
 
+                // Current Weather & Conditions //
                 currentTemperature = (double)weatherData["main"]["temp"];
                 currentHumidity = (double)weatherData["main"]["humidity"];
                 currentPressure = (double)weatherData["main"]["pressure"];
                 weatherConditions = (string)weatherData["weather"][0]["description"];
-                weatherConditionsIMG = (string)weatherData["weather"][0]["icon"];
-
+                weatherICON = (string)weatherData["weather"][0]["icon"];
             }
         }
     }
 }
-
--- WPF Weather Forcast Application/MainWindow.xaml.cs
+++ WPF Weather Forcast Application/MainWindow.xaml.cs
@@ -8,25 +8,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 
 namespace WPF_Weather_Forcast_Application
 {
     public partial class MainWindow : Window
     {
+
+        private static string lat = "36.1881";
+        private static string lon = "-115.1765";
+        string city = "London";
+        public static string Lat { get { return lat; } set { lat = value; } }
+        public static string Lon { get { return lon; } set { lon = value; } }
+
         public MainWindow()
         {
             InitializeComponent();
-            UpdateWeather();
+            CurrentWeather();
+            Forcast();
         }
-        private async void UpdateWeather()
+        private async void CurrentWeather()
         {
-            OpenWeatherAPI openWeatherAPI = new OpenWeatherAPI();
-            await openWeatherAPI.FetchCurrentWeather();
-            LBL_City.Content = openWeatherAPI.City;
-            LBL_CurrentTemp.Content = openWeatherAPI.CurrentTemperature + "°C";
-            LBL_CurrentHumidity.Content = "Humidity: " + openWeatherAPI.CurrentHumidity + "%";
-            LBL_CurrentPressure.Content = "Air Pressure: " + openWeatherAPI.CurrentPressure + " hpa";
-            LBL_WeatherConditions.Content = openWeatherAPI.WeatherConditions;
+            CurrentWeather CurrentWeather = new CurrentWeather();
+            await CurrentWeather.FetchCurrentWeather(lat, lon);
+
+            LBL_City.Content = city;
+            LBL_CurrentTemp.Content = CurrentWeather.CurrentTemperature + "°C";
+            LBL_CurrentHumidity.Content = "Humidity: " + CurrentWeather.CurrentHumidity + "%";
+            LBL_CurrentPressure.Content = "Air Pressure: " + CurrentWeather.CurrentPressure + " hpa";
+            LBL_WeatherConditions.Content = CurrentWeather.WeatherConditions;
+
+            string iconUrl = $"http://openweathermap.org/img/w/{CurrentWeather.WeatherICON}.png";
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(iconUrl, UriKind.Absolute);
+            bitmap.EndInit();
+            IMG_CurrentConditions.Source = bitmap;
+        }
+        private async void Forcast()
+        {
 
         }
     }
